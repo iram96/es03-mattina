1b2a0965ab87f0f389775244acd7a9cc7c061299
iram96 <mariano.maselli@virgilio.it>
2022-05-12 14:44:41 +0200

    Aggiungere i file di progetto.

---
+++ CodeFile1.cs
@@ -0,0 +1,47 @@
+﻿namespace MyApp
+{
+    class ShowMessage
+    {
+        int[] viMioVettore;
+
+        private static string sMiaStringa = "ciao";
+
+        private int sLunghezzaStringa = sMiaStringa.Length;
+        public ShowMessage()
+        {
+            Console.WriteLine("Hi, I am default message");
+            //this.viMioVettore = new int[200];
+        }
+
+        public ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
+        // passo uno stringone che ogni 10 char faccio andare a capo
+        public void AppendMessage(string sMiaStringa)
+        {
+            int pos = 0;
+            foreach (char c in sMiaStringa)
+                if (pos == 10)
+                {
+                    Console.WriteLine();
+                    pos = 0;
+                }
+                else
+                {
+                    Console.WriteLine(c);
+                    pos++;
+
+                }
+        }
+
+        ~ShowMessage()
+        {
+            Console.WriteLine("I am destructor and I clean the resources and free up memory as soon as program closed");
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MyApp
+{
+    public class Program
+    {
+
+        static void Metodo(string sString)
+        {
+            MyApp.ShowMessage smMioMessaggio = new MyApp.ShowMessage();
+
+            Console.WriteLine("ciao");
+        }
+        static void Main(string[] args)
+        {
+
+            Metodo("Ciao");
+
+
+            //Il tipo DateTime
+            /*
+             * TimeSpan è un struct, che identifica una struttura dati,
+             * composta da giorni, ore, minuti, secondi e millisecondi
+             * 
+             */
+            DateTime dt1 = new DateTime(2022, 12, 20);
+            DateTime dt2 = new DateTime(2021, 12, 31, 5, 10, 20);
+            TimeSpan time = new TimeSpan(10, 5, 25, 50);
+
+            Console.WriteLine(dt2 + time); // 1/10/2022 10:36:10 AM
+
+
+
+        }
+    }
+}
